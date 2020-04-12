0ee52bfd0d84fd9da48489915ed28cf2ad3db8bf
bouldercoder9 <alma9011@colorado.edu>
2020-04-12 01:37:05 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+  /* App requirements section */
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const session = require('express-session');
+const bodyParser= require('body-parser');
+  
+/* Declare express app */
+const app = express();
+  
+/* App use section */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('//', express.static('resources'))
+
+  
+/* Sets the view engine to ejs, this is just an html templating thing. */
+app.set('view engine', 'ejs');
+  
+/* App is running on port 3000 */
+app.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
